e786fd7a2748e6d1e61187295b931c3a3c7a0f41
Grace <grace.adelaka@charisol.io>
2024-10-21 22:48:17 +0100

    implemented new entry button and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { AppNavbar } from "./navbar/navbar.component";
 import { UsersComponent } from './users/users.component';
 import { CommentsComponent } from './comments/comments.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AddUserComponent } from './users/add-user/add-user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppNavbar, SidebarComponent ,UsersComponent, CommentsComponent],
+  imports: [RouterOutlet, AppNavbar, SidebarComponent ,UsersComponent, CommentsComponent, AddUserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [AddUserComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
