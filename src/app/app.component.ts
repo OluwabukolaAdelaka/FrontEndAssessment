b7a831c3ce37a7428f1b77cde38e64ccfdb91e74
Grace <grace.adelaka@charisol.io>
2024-10-21 21:40:57 +0100

    Add components, display and style navbar and sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavbar } from "./navbar/navbar.component";
+import { UsersComponent } from './users/users.component';
+import { CommentsComponent } from './comments/comments.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppNavbar, SidebarComponent ,UsersComponent, CommentsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-navbar",
+    standalone: true,
+    template: "<h1>Navbarrrr Component...</h1>",
+    templateUrl: "./navbar.component.html",
+    styleUrl: "./navbar.component.css"
+})
+
+export class AppNavbar {}
